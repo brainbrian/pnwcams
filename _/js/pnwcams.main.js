dd8f52d14cd8b4de8b8f96c70e081ab6caae9777
BrainBrian <brian@brainbrian.com>
2014-11-04 19:43:31 -0800

    removed scroll magic and added carousel for each section

--- _/js/pnwcams.main.js
+++ _/js/pnwcams.main.js
@@ -8,66 +8,19 @@ var PNWCAMS = PNWCAMS || {};
 PNWCAMS.main = {
 	config: {},
 	init: function () {
-		var self, controller, tween, scene;
+		var self, carousel;
 		self = this;
-		controller = new ScrollMagic({vertical: true});
-		// set up scenes/tweens
-		tween = new TweenMax.to("#photo1", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title1", duration: $(window).height()*2}).setTween(tween).addTo(controller);
 
-		tween = new TweenMax.to("#photo2", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title2", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo3", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title3", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo4", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title4", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo5", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title5", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo6", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title6", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo7", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title7", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo8", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title8", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo9", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title9", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo10", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title10", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo11", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title11", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo12", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title12", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo13", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title13", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo14", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title14", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo15", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title15", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo16", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title16", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo17", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title17", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-
-		tween = new TweenMax.to("#photo18", 1, {top: "-50%", display: 'block', ease: Linear.easeNone});
-		scene = new ScrollScene({triggerElement: "#title18", duration: $(window).height()*2}).setTween(tween).addTo(controller);
-		
-		// show indicators (requires debug extension)
-		//scene.addIndicators();
+		// set up owl carousel
+		carousel = $(".cameras").owlCarousel({
+			items: 1,
+			dots: true,
+			lazyLoad: true,
+			autoplay: false,
+			autoplayTimeout: 8000,
+			autoplayHoverPause: true,
+			loop: true
+		});
 	},
 	utilities: {
 		cookie: {
@@ -100,35 +53,6 @@ PNWCAMS.main = {
 			}, 'slow', function () {
 				window.location = hash;
 			});
-		},
-		getMediaWidth: function () {
-			var self = this,
-				width;
-			// Check on this with gavin
-			/*
-            if (typeof matchMedia !== 'undefined') {
-                width = self.bruteForceMediaWidth();
-            } else {
-            */
-			width = window.innerWidth || document.documentElement.clientWidth;
-			//}
-			return width;
-		},
-		bruteForceMediaWidth: function () {
-			var i = 0,
-				found = false;
-			while (!found) {
-				if (matchMedia('(width: ' + i + 'px)').matches) {
-					found = true;
-				} else {
-					i++;
-				}
-				// Prevent infinite loop if something goes horribly wrong
-				if (i === 9999) {
-					break;
-				}
-			}
-			return i;
 		}
 	}
 };
--- index.php
+++ index.php
@@ -14,140 +14,243 @@
 	<div id="snow" class="section-card">
 		<h1>Snow Cams</h1>
 	</div>
-	<div id="title1" class="title-card">
-		<h2>Stevens Pass</h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>Stevens Pass</h2>
+			<!-- http://www.stevenspass.com/site/mountain/cams -->
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://www.stevenspass.com/cams/base1/?<?php echo time(); ?>">
+				<h3><span>Base</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.stevenspass.com/cams/mountain/?<?php echo time(); ?>">
+				<h3><span>Mountain</span></h3>
+			</div>
+		</div>
 	</div>
-	<div class="spacer">
-		<h3><span>Base Camera</span></h3>
+	<div class="location">
+		<div class="title-card">
+			<h2>Snoqualmie Pass</h2>
+			<!-- http://www.summitatsnoqualmie.com/conditions -->
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://www.summitatsnoqualmie.com/webcams/Alpcam.jpg?<?php echo time(); ?>">
+				<h3><span>Alpental Base</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.summitatsnoqualmie.com/webcams/Centcam.jpg?<?php echo time(); ?>">
+				<h3><span>Summit Central</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.summitatsnoqualmie.com/webcams/silverfir.jpg?<?php echo time(); ?>">
+				<h3><span>Silver Fir</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://images.wsdot.wa.gov/sc/090VC05200.jpg?<?php echo time(); ?>">
+				<h3><span>DOT Snoqualmie</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://images.wsdot.wa.gov/sc/090VC05517.jpg?<?php echo time(); ?>">
+				<h3><span>DOT Hyak</span></h3>
+			</div>
+		</div>
 	</div>
-	<div id="photo1" class="photo-card" style="background-image: url('http://www.stevenspass.com/cams/base1/?<?php echo time(); ?>');"></div>
-	<!-- http://www.stevenspass.com/cams/mountain/ -->
-	<div id="title2" class="title-card">
-		<h2>Snoqualmie Pass</h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>Crystal Mountain</h2>
+			<!-- http://crystalmountainresort.com/the-mountain/about/webcams/ -->
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://wwc.instacam.com/instacamimg/CRYMB/CRYMB_l.jpg?<?php echo time(); ?>">
+				<h3><span>Mt. Rainier Gondola</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://wwc.instacam.com/instacamimg/CRYM4/CRYM4_l.jpg?<?php echo time(); ?>">
+				<h3><span>Rainier Express</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://crystalmountainresort.com/webcam/goldhillscam.jpg?<?php echo time(); ?>">
+				<h3><span>Gold Hills</span></h3>
+			</div>
+		</div>
 	</div>
-	<div class="spacer">
-		<h3><span>Silver Fir</span></h3>
+	<div class="location">
+		<div class="title-card">
+			<h2>Mt. Rainier</h2>
+			<!-- http://www.nps.gov/mora/photosmultimedia/webcams.htm -->
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://www.nps.gov/webcams-mora/mountain.jpg?<?php echo time(); ?>">
+				<h3><span>Paradise - Mountain</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.nps.gov/webcams-mora/east.jpg?<?php echo time(); ?>">
+				<h3><span>Paradise - Parking East</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.nps.gov/webcams-mora/west.jpg?<?php echo time(); ?>">
+				<h3><span>Paradise - Parking West</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.nps.gov/webcams-mora/west.jpg?<?php echo time(); ?>">
+				<h3><span>Paradise - Parking West</span></h3>
+			</div>
+
+			<div class="camera owl-lazy" data-src="http://www.nps.gov/webcams-mora/gh.jpg?<?php echo time(); ?>">
+				<h3><span>Paradise - Visitor Center</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.nps.gov/webcams-mora/tatoosh.jpg?<?php echo time(); ?>">
+				<h3><span>Paradise - Tatoosh</span></h3>
+			</div>
+		</div>
 	</div>
-	<div id="photo2" class="photo-card" style="background-image: url('http://www.summitatsnoqualmie.com/webcams/silverfir.jpg?<?php echo time(); ?>');"></div>
-	<div id="title3" class="title-card">
-		<h2>Crystal Mountain</h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>Hurricane Ridge</h2>
+			<!-- http://www.nps.gov/olym/photosmultimedia/hurricane-ridge-webcam.htm -->
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://www.nps.gov/webcams-olym/current_ridgecam.jpg?<?php echo time(); ?>">
+				<h3><span>Ridge</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.nps.gov/webcams-olym/northcam.jpg?<?php echo time(); ?>">
+				<h3><span>Parking</span></h3>
+			</div>
+		</div>
 	</div>
-	<div class="spacer">
-		<h3><span>Mt. Rainier Gondola</span></h3>
-	</div>
-	<div id="photo3" class="photo-card" style="background-image: url('http://wwc.instacam.com/instacamimg/CRYMB/CRYMB_l.jpg?<?php echo time(); ?>');"></div>
-	<div id="title4" class="title-card">
-		<h2>Mt. Rainier</h2>
-	</div>
-	<div class="spacer">
-		<h3><span>Paradise - Uppper Parking Lot</span></h3>
-	</div>
-	<div id="photo4" class="photo-card" style="background-image: url('http://icons.wunderground.com/webcamramdisk/s/h/shauntanner/39/current.jpg?<?php echo time(); ?>');"></div>
-<!-- Paradise - Mtn. &amp; Meadows
-http://icons.wunderground.com/webcamramdisk/s/h/shauntanner/38/current.jpg -->
-	<div id="title5" class="title-card">
-		<h2>Hurricane Ridge</h2>
-	</div>
-	<div class="spacer">
-		<h3><span>Ridge</span></h3>
-	</div>
-	<div id="photo5" class="photo-card" style="background-image: url('http://www.nps.gov/webcams-olym/current_ridgecam.jpg?<?php echo time(); ?>');"></div>
 	<div id="surf" class="section-card">
 		<h1>Surf Cams</h1>
 	</div>
-	<div id="title6" class="title-card">
-		<h2>Westport</h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>Westport</h2>
+			<!-- http://www.westportgrayland-chamber.org/surfing_info.php & http://www.rainbowsendcam.com/webcam32.htm -->
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://www.westportcam.com/images/position3.jpg?<?php echo time(); ?>">
+				<h3><span>The Groins - Left</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.westportcam.com/images/position4.jpg?<?php echo time(); ?>">
+				<h3><span>The Groins - Right</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.rainbowsendcam.com/images/rainbowsendcam.jpg?<?php echo time(); ?>">
+				<h3><span>The Jetty</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.westportcam.com/images/position1.jpg?<?php echo time(); ?>">
+				<h3><span>Half Moon Bay - Left</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.westportcam.com/images/position2.jpg?<?php echo time(); ?>">
+				<h3><span>Half Moon Bay - Right</span></h3>
+			</div>
+		</div>
 	</div>
-	<div class="spacer">
-		<h3><span>The Groins</span></h3>
+	<div class="location">
+		<div class="title-card">
+			<h2>Grays Harbor</h2>
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://www.wrh.noaa.gov/images/mfr/camera/GraysH.jpg?<?php echo time(); ?>">
+				<h3></h3>
+			</div>
+		</div>
 	</div>
-	<div id="photo6" class="photo-card" style="background-image: url('http://www.westportcam.com/images/position3.jpg?<?php echo time(); ?>');"></div>
-	<div id="title7" class="title-card">
-		<h2>Grays Harbor</h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>Ocean Shores</h2>
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://63.228.108.193/netcam.jpg?<?php echo time(); ?>">
+				<h3></h3>
+			</div>
+		</div>
 	</div>
-	<div class="spacer"></div>
-	<div id="photo7" class="photo-card" style="background-image: url('http://www.wrh.noaa.gov/images/mfr/camera/GraysH.jpg?<?php echo time(); ?>');"></div>
-	<div id="title8" class="title-card">
-		<h2>Ocean Shores</h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>La Push</h2>
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://webcam.forkswa.com/webcam1/netcam.jpg?<?php echo time(); ?>">
+				<h3><span>First Beach</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://pixelcaster.com/dnc-kalaloch/kalaloch.jpg?<?php echo time(); ?>">
+				<h3><span>Forks - Kalaloch Lodge</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://www.wrh.noaa.gov/images/mfr/camera/QRiver.jpg?<?php echo time(); ?>">
+				<h3><span>Q River</span></h3>
+			</div>
+		</div>
 	</div>
-	<div class="spacer"></div>
-	<div id="photo8" class="photo-card" style="background-image: url('http://63.228.108.193/netcam.jpg?<?php echo time(); ?>');"></div>
-	<div id="title9" class="title-card">
-		<h2>La Push</h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>The Straight</h2>
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://hoko.pnwas.org/images/webcam/hoko/current.jpg?<?php echo time(); ?>">
+				<h3><span>Hoko River</span></h3>
+			</div>
+			<div class="camera owl-lazy" data-src="http://icons.wunderground.com/webcamramdisk/s/e/SequimFlyer/1/current.jpg?<?php echo time(); ?>">
+				<h3><span>Blue Ribbon Farms</span></h3>
+			</div>
+		</div>
 	</div>
-	<div class="spacer">
-		<h3><span>First Beach</span></h3>
+	<div class="location">
+		<div class="title-card">
+			<h2>Seaside</h2>
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://68.185.24.174/-wvhttp-01-/image.cgi?v=jpg:640x480&time=<?php echo time(); ?>">
+				<h3><span>The Cove</span></h3>
+			</div>
+		</div>
 	</div>
-	<div id="photo9" class="photo-card" style="background-image: url('http://webcam.forkswa.com/webcam1/netcam.jpg?<?php echo time(); ?>');"></div>
-	<div id="title10" class="title-card">
-		<h2>Forks - Kalaloch Lodge</h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>Cannon Beach</h2>
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://wwc.instacam.com/instacamimg/CNHNB/CNHNB_l.jpg?<?php echo time(); ?>">
+				<h3><span>Haystack Rock</span></h3>
+			</div>
+		</div>
 	</div>
-	<div class="spacer"></div>
-	<div id="photo10" class="photo-card" style="background-image: url('http://pixelcaster.com/dnc-kalaloch/kalaloch.jpg?<?php echo time(); ?>');"></div>
-	<div id="title11" class="title-card">
-		<h2>Q River</h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>Manzanita</h2>
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://www.weathercam.neahkahnie.net/netcam.jpg?<?php echo time(); ?>">
+				<h3></h3>
+			</div>
+		</div>
 	</div>
-	<div class="spacer"></div>
-	<div id="photo11" class="photo-card" style="background-image: url('http://www.wrh.noaa.gov/images/mfr/camera/QRiver.jpg?<?php echo time(); ?>');"></div>
-	<div id="title12" class="title-card">
-		<h2>Hoko River</h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>Oceanside</h2>
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera" data-src="http://www.oceansideoregon.com/cam.jpg?<?php echo time(); ?>">
+				<h3><span>The Three Arch Rocks</span></h3>
+			</div>
+		</div>
 	</div>
-	<div class="spacer"></div>
-	<div id="photo12" class="photo-card" style="background-image: url('http://hoko.pnwas.org/images/webcam/hoko/current.jpg?<?php echo time(); ?>');"></div>
-	<!-- <div id="title13" class="title-card">
-		<h2>Sequim</h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>Pacific City</h2>
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://archive.kgw.com/weather/images/core/webcams/pacific-city.jpg?<?php echo time(); ?>">
+				<!-- http://belo.bimedia.net/KGW/weather/stills/pacific-city.jpg -->
+				<h3><span>Cape Kiwanda</span></h3>
+			</div>
+		</div>
 	</div>
-	<div class="spacer">
-		<h3><span>Blue Ribbon Farms</span></h3>
-	</div>
-	<div id="photo13" class="photo-card" style="background-image: url('http://icons.wunderground.com/webcamramdisk/s/e/SequimFlyer/1/current.jpg');"></div> --></body>
-	<div id="title13" class="title-card">
-		<h2>Seaside Cove</h2>
-	</div>
-	<div class="spacer"></div>
-	<div id="photo13" class="photo-card" style="background-image: url('http://68.185.24.174/-wvhttp-01-/image.cgi?v=jpg:640x480&time=<?php echo time(); ?>');"></div>
-	<div id="title14" class="title-card">
-		<h2>Cannon Beach</h2>
-	</div>
-	<div class="spacer">
-		<h3><span>Haystack Rock</span></h3>
-	</div>
-	<div id="photo14" class="photo-card" style="background-image: url('http://wwc.instacam.com/instacamimg/CNHNB/CNHNB_l.jpg?<?php echo time(); ?>');"></div>
-	<div id="title15" class="title-card">
-		<h2>Manzanita</h2>
-	</div>
-	<div class="spacer"></div>
-	<div id="photo15" class="photo-card" style="background-image: url('http://www.weathercam.neahkahnie.net/netcam.jpg?<?php echo time(); ?>');"></div>
-	<div id="title16" class="title-card">
-		<h2>Oceanside</h2>
-	</div>
-	<div class="spacer">
-		<h3><span>The Three Arch Rocks</span></h3>
-	</div>
-	<div id="photo16" class="photo-card" style="background-image: url('http://www.oceansideoregon.com/cam.jpg?<?php echo time(); ?>');"></div>
-	<div id="title17" class="title-card">
-		<h2>Pacific City</h2>
-	</div>
-	<div class="spacer"></div>
-	<div id="photo17" class="photo-card" style="background-image: url('http://archive.kgw.com/weather/images/core/webcams/pacific-city.jpg?<?php echo time(); ?>');"></div>
-	<!-- http://belo.bimedia.net/KGW/weather/stills/pacific-city.jpg -->
-	<div id="title18" class="title-card">
-		<h2>Lincoln City</h2>
-	</div>
-	<div class="spacer">
-		<h3><span>Road's End</span></h3>
-	</div>
-	<div id="photo18" class="photo-card" style="background-image: url('http://www.lcsurfshop.com/webcam32.jpg?<?php echo time(); ?>');"></div>
-	<div class="title-card">
-		<h2>Brought to you by <a href="http://www.brainbrian.com">brainbrian.com</a></h2>
+	<div class="location">
+		<div class="title-card">
+			<h2>Lincoln City</h2>
+		</div>
+		<div class="cameras owl-carousel owl-theme">
+			<div class="camera owl-lazy" data-src="http://www.lcsurfshop.com/webcam32.jpg?<?php echo time(); ?>">
+				<h3><span>Road's End</span></h3>
+			</div>
+		</div>
 	</div>
 	<div class="section-card">
-		<h1>The End</h1>
+		<h1>Brought to you by <a href="http://www.brainbrian.com">brainbrian.com</a></h1>
 	</div>
-	<script type="text/javascript" src="_/js/lib/greensock/TweenMax.min.js"></script>
 	<script type="text/javascript" src="_/js/lib/jquery-1.11.0.min.js"></script>
   	<script type="text/javascript" src="_/js/lib/modernizr.custom.min.js"></script>
-  	<script type="text/javascript" src="_/js/lib/jquery.scrollmagic.min.js"></script>
+  	<script type="text/javascript" src="_/js/lib/owl.carousel-2.0.0.js"></script>
   	<script type="text/javascript" src="_/js/pnwcams.main.js"></script>
   	<script type="text/javascript">
 		$(document).ready(function(){
