d4e411835736ef97938637da0f0eda0d69ef7ce6
Brian Behrens <brian.behrens@popagency.com>
2015-12-13 04:16:03 -0800

    Add top nav, build location templates from json
    - add top nav with deeplinking based on hash
    - add location templating with handlebarsjs
    - load json data upfront and populate page content

--- src/scripts/modules/Application.js
+++ src/scripts/modules/Application.js
@@ -1,12 +1,65 @@
+import locationTempalte from 'templates/location.hbs';
+import _ from 'underscore';
+
 /**
  * PNW Cams - http://www.pnwcams.com
  * Author: brian@brainbrian.com - http://www.brainbrian.com
  */
 
 const Application = {
-  config: {},
+  config: {
+    data: null
+  },
+  ui: {},
 
   init: function() {
+    this._requestData();
+    this.ui = {
+      locations: $('#locations'),
+      navBtns: $('.header__nav-btn'),
+      cameras: null
+    };
+  },
+
+  _requestData: function() {
+    $.ajax({
+      dataType: "json",
+      url: '/assets/json/data.json',
+      success: function(data) {
+        var hash = window.location.hash.replace('#', '');
+        var cat = 'snow';
+        if(hash === 'snow' || hash === 'surf') cat = hash;
+        this.config.data = data;
+        this._bindEvents();
+        this._buildLocations(this.config.data, cat);
+      }.bind(this)
+    });
+  },
+
+  _bindEvents: function() {
+    this.ui.navBtns.on('click', function(e) {
+      var target = e.target,
+        $parent = $(target).parent(),
+        category = $parent.data('cat');
+      if(this.config.category !== category) this._buildLocations(this.config.data, category);
+    }.bind(this));
+  },
+
+  _buildLocations: function(data, category) {
+    var locationHtml;
+    this.ui.navBtns.removeClass('header__nav-btn--active');
+    if(category === 'surf') {
+      locationHtml = locationTempalte({'locations': _.where(data.locations, {category: "surf"})});
+      $('.header__nav-btn[data-cat="surf"]').addClass('header__nav-btn--active');
+    } else {
+      // default to snow
+      locationHtml = locationTempalte({'locations': _.where(data.locations, {category: "snow"})});
+      $('.header__nav-btn[data-cat="snow"]').addClass('header__nav-btn--active');
+    }
+    this._destroyCarousels();
+    this.ui.locations.html('');
+    this.ui.locations.html(locationHtml);
+    this.ui.cameras = this.ui.locations.find('.cameras');
     this._randomImgLoad();
     this._buildCarousels();
   },
@@ -23,7 +76,7 @@ const Application = {
 
   _buildCarousels: function() {
       // set up owl carousels
-      $(".cameras").owlCarousel({
+      this.ui.cameras.owlCarousel({
         items: 1,
         dots: true,
         lazyLoad: true,
@@ -32,6 +85,10 @@ const Application = {
         autoplayHoverPause: true,
         loop: true
       });
+  },
+
+  _destroyCarousels: function() {
+      if(this.ui.cameras !== null) this.ui.cameras.trigger('destroy.owl.carousel');
   },
 
   utilities: {
